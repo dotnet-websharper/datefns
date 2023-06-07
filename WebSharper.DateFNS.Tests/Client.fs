5811a5266968a1067389b8b79139826b7976b11b
jooseppi12 <urijozsef@europe.com>
2023-06-07 13:32:36 +0200

    #8 - Add PR acceptance test

--- WebSharper.DateFNS.Tests/Client.fs
+++ WebSharper.DateFNS.Tests/Client.fs
@@ -2,49 +2,40 @@ namespace WebSharper.DateFNS.Tests
 
 open WebSharper
 open WebSharper.JavaScript
-open WebSharper.UI
-open WebSharper.UI.Html
-open WebSharper.UI.Client
-open WebSharper.UI.Templating
 open WebSharper.DateFNS
 
 [<JavaScript>]
-module Client =
-    // The templates are loaded from the DOM, so you just can edit index.html
-    // and refresh your browser, no need to recompile unless you add or remove holes.
-    type IndexTemplate = Template<"index.html", ClientLoad.FromDocument>
+module Testing =
 
-    [<SPAEntryPoint>]
+    type Chai =
+        [<Name "assert">]
+        abstract ``assert`` : Assert
+
+    and [<Name "">] Assert =
+        abstract equal: obj * obj -> unit
+
+    type Mocha =
+        [<Inline "$global.describe($category, $fn)">]
+        static member describe (category: string, fn: (unit -> unit)) = X<unit>
+        [<Inline "$global.it($testName, $fn)">]
+        static member it(testName: string, fn: (unit -> unit)) = X<unit>
+
+    let mocha () : Mocha = JS.ImportDefault "mocha"
+    let chai () : Chai = JS.ImportDefault "chai"
+
+[<JavaScript>]
+module Tests =
     let Main () =
-        let newName = Var.Create ""
-        let date = DateFNS.Format(Date(2014, 1, 11), "MM/dd/yyyy")
-        let nextD = DateFNS.NextDay(Date(), 0).ToString()
-        let nextM = DateFNS.NextMonday(Date()).ToString()
-        let formatOpt = FormatOptions()
-        formatOpt.Locale <- Locales.JA
-        let localeDate = DateFNS.Format(Date(), "MMM dd yyyy", formatOpt)
-        let defaultDate = DateFNS.Format(Date(), "MMM dd yyyy")
-        IndexTemplate.Main()
-            .FormatDate(
-                Doc.Concat [
-                    h2 [] [text "Date formating"]
-                    p [] [text date]
-                ]
+        let mocha = Testing.mocha ()
+        let chai = Testing.chai ()
+        Testing.Mocha.describe("DateFNS", fun () ->
+            Testing.Mocha.describe("format", fun () ->
+                Testing.Mocha.it("format as MM/dd/yyyy", fun () ->
+                    let date = Date(2014, 0, 11)
+                    chai.``assert``.equal(DateFNS.Format(date, "MM/dd/yyyy"), "01/11/2014")
+                )   
             )
-            .NextDay(
-                Doc.Concat [
-                    h2 [] [text "Next Day (Sunday) test"]
-                    p [] [text nextD]
-                    h2 [] [text "Next Monday test"]
-                    p [] [text nextM]
-                ]
-            )
-            .LocaleTest(
-                Doc.Concat [
-                    h2 [] [text "Locale Test"]
-                    p [] [text ("With default locale: " + defaultDate)]
-                    p [] [text ("With Japanese locale: "+ localeDate)]
-                ]
-            )
-            .Doc()
-        |> Doc.RunById "main"
+        )
+
+    Main ()
+
